f3b8da2bb0abcb1b28bd2e0509c3c9cfd0d62ca6
Rosana Rezende <rezende_rosana@hotmail.com>
2020-03-14 14:23:42 -0300

    Finalizado módulo: iniciando com react

--- iniciando-com-react/src/App.js
+++ iniciando-com-react/src/App.js
@@ -1,26 +1,85 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+// class App extends React.Component {
+
+//   // 11
+//   // state = {
+//   //   nome: ""
+//   // }
+//   // mudificarNome = (event) => {
+//   //   this.setState({ nome: event.target.value })
+//   // }
+
+//   // 12
+//   // criaComboBox = () => {
+//   //   const opcoes = ['Fulano', 'Cicrano']
+//   //   const comboBoxOpcoes = opcoes.map( opcao => <option>{opcao}</option>)
+//   //   return (
+//   //     <select>
+//   //       {comboBoxOpcoes}
+//   //     </select>
+//   //   )
+//   // }
+
+//   // 14
+//   // componentDidMount() {
+//   //   console.log('Executou o componentDidMount')
+//   // }
+
+//   render() {
+//     // 14
+//     // console.log('Executou o render')
+
+//     // 12
+//     // const MeuComboBox = () => this.criaComboBox()
+
+//     return (
+//       <>
+//         {/* 11 */}
+//         {/* <input type='text' value={this.state.nome} onChange={this.mudificarNome} />
+//         <h1> Oi {this.state.nome}</h1> */}
+
+//         {/* 12 */}
+//         {/* <MeuComboBox/> */}
+
+//         {/* 13 - props */}
+//         {/* <h1> {this.props.nome} tem {this.props.idade} anos de idade</h1> */}
+
+//       </>
+//     )
+//   }
+// }
+
+
+// 15 - componente funcional
+function App(props) {
+
+  const modificarNome = (event) => {
+   console.log(event.target.value)
+  }
+
+  // podemos ter funções, mas agora dentro de uma const, q recebe uma arrow function
+  const criaComboBox = () => {
+    const opcoes = ['Fulano', 'Cicrano']
+    const comboBoxOpcoes = opcoes.map((opcao, index) => <option key={index}>{opcao}</option>)
+    return (
+      <select>
+        <option hidden></option>
+        {comboBoxOpcoes}
+      </select>
+    )
+  }
+
+  // Tb posso ter o método, mas agora sem o this
+  const MeuComboBox = () => criaComboBox()
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <input className='texto-centralizado' type='text' value={props.nome} onChange={modificarNome} />
+    <h1> {props.nome} tem {props.idade} anos de idade</h1>
+    <MeuComboBox/>
+    </>
+  )
 }
 
 export default App;
