ad1c73de82e71ccef4a2212a559fccf7631bf6aa
Rosana Rezende <rezende_rosana@hotmail.com>
2020-03-15 15:48:22 -0300

    Tela home - informações de autoria

--- cadastro-produtos/src/views/home.js
+++ cadastro-produtos/src/views/home.js
@@ -9,7 +9,8 @@ function Home() {
             </p>
             <hr className="my-4"/>
             <p className="lead">
-                <a className="btn btn-primary btn-lg" href="#" role="button">Cadastrar</a>
+                {/* <a className="btn btn-primary btn-lg" href="#" role="button">Cadastrar</a> */}
+                Crud desenvolvido por <a href='https://github.com/rosanarezende'>Rosana Rezende</a> durante <a href='https://www.udemy.com/course/domine-react-com-redux-2020-bootstrap-material-ui-e-apis-rest/'>curso</a> do professor Dougllas Sousa na Udemy
             </p>
         </div>
     )
