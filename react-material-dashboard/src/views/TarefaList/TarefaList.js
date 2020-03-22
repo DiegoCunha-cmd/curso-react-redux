be66850df99147b34c6c3ebe4b808fe8c8b53bd7
Rosana Rezende <rezende_rosana@hotmail.com>
2020-03-22 13:07:21 -0300

    Dialog

--- react-material-dashboard/src/views/TarefaList/TarefaList.js
+++ react-material-dashboard/src/views/TarefaList/TarefaList.js
@@ -1,9 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/styles';
+import axios from 'axios'
 
 import { TarefasToolbar, TarefasTable } from './components';
 
-import axios from 'axios'
+import { 
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  Button
+} from '@material-ui/core';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -21,6 +28,8 @@ const TarefaList = () => {
 
   // const [tarefas] = useState([]); // não tem info pq passei um array vazio
   const [tarefas, setTarefas] = useState([]);
+  const [openDialog, setOpenDialog] = useState(false)
+  const [mensagem, setMensagem] = useState('')
 
   const API_URL = 'https://minhastarefas-api.herokuapp.com/tarefas'
   const headers = { 'x-tenant-id': 'rosana@email.com' }
@@ -38,9 +47,12 @@ const TarefaList = () => {
         // OTIMIZAÇÃO - se tiver muitas tarefas, não chamar todas novamente
         const novaTarefa = response.data
         setTarefas([...tarefas, novaTarefa]) // só adiciono as tarefas já existentes
-
+        setMensagem('Item adicionado com sucesso!')
+        setOpenDialog(true)
       }).catch(erro => {
         console.log(erro)
+        setMensagem('Ocorreu um erro ao adicionar tarefa. Tente novamente mais tarde!')
+        setOpenDialog(true)
       })
   }
 
@@ -55,6 +67,8 @@ const TarefaList = () => {
         // console.log(listaDeTarefas)
       }).catch(erro => {
         console.log(erro)
+        setMensagem('Ocorreu um erro ao listar tarefas. Tente novamente mais tarde!')
+        setOpenDialog(true)
       })
   }
 
@@ -73,8 +87,12 @@ const TarefaList = () => {
           }
         })
         setTarefas(lista)
+        setMensagem('Status do item atualizado com sucesso!')
+        setOpenDialog(true)
       }).catch(erro => {
         console.log(erro)
+        setMensagem('Ocorreu um erro ao alterar status da tarefa. Tente novamente mais tarde!')
+        setOpenDialog(true)
       })
   }
 
@@ -89,8 +107,12 @@ const TarefaList = () => {
         // console.log(response.status)
         const lista = tarefas.filter(tarefa => tarefa.id !== id)
         setTarefas(lista)
+        setMensagem('Item removido com sucesso!')
+        setOpenDialog(true)
       }).catch(erro => {
         console.log(erro)
+        setMensagem('Ocorreu um erro ao deletar tarefa. Tente novamente mais tarde!')
+        setOpenDialog(true)
       })
   }
 
@@ -110,6 +132,16 @@ const TarefaList = () => {
           deletarTarefa={deletarTarefa}
         />
       </div>
+      <Dialog open={openDialog} onClose={() => setOpenDialog(false)}
+      >
+        <DialogTitle>Atenção</DialogTitle>
+        <DialogContent>
+          {mensagem}
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => setOpenDialog(false)}>fechar</Button>
+        </DialogActions>
+      </Dialog>
     </div>
   );
 };
