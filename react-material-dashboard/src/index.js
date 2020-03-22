12fde8aef5e897a5c8dccbcdf8fc1fb11ee39849
Rosana Rezende <rezende_rosana@hotmail.com>
2020-03-21 22:32:28 -0300

    Axios

--- react-material-dashboard/src/index.js
+++ react-material-dashboard/src/index.js
@@ -4,6 +4,15 @@ import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 import App from './App';
 
+// import axios from 'axios'
+
+// TESTANDO INTEGRAÇÃO COM API
+// axios.get('https://minhastarefas-api.herokuapp.com/tarefas', {
+//     headers: { 'x-tenant-id': 'rosana@email.com' }
+// }).then( resposta => {
+//     console.log(resposta.data)
+// })
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 serviceWorker.unregister();
